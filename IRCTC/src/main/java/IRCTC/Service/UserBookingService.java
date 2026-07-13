0de4d71dff0456627a39690dbd9cdb716ffdf1f9
kartikeyofficial <kartikeygithubproject@gmail.com>
2026-07-13 23:08:14 +0530

    IRCTC Backend Logics

--- IRCTC/src/main/java/IRCTC/Service/UserBookingService.java
+++ IRCTC/src/main/java/IRCTC/Service/UserBookingService.java
@@ -1,11 +1,27 @@
 package IRCTC.Service;
 
+import IRCTC.Entities.user;
 import org.apache.catalina.User;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.util.List;
 
 public class UserBookingService {
     private User user;
+
+    private  List<user> userList;
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final String USER_PATH="../LocalDB/users.json";
+
     public UserBookingService(User user){
           this.user = user;
+          File users = new File(USER_PATH);
+          userList = objectMapper.readValue(users, new TypeReference<List<User>>() {
+
+          });
     }
 
 
